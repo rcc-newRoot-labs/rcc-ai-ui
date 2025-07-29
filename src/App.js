a6134c3b6f8ab2cc4a921e722073b78c1ebc4407
usha yerramsetti <ushay.webdev@gmail.com>
2025-07-29 08:57:58 -0500

    updated app, index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const isAuthenticated = localStorage.getItem('token'); // or your auth logic
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route
+          path="/dashboard"
+          element={<DashboardPage/>}
+        />
+      </Routes>
+    </Router>
   );
 }
 
