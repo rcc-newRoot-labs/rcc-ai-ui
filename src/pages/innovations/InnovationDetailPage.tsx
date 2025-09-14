82d58531f384c75dc271c8faecd55facf27a16d0
spkotalaRcc <surya.kotala@redcedarconsultancy.com>
2025-09-14 18:51:57 -0400

    added border style

--- src/pages/innovations/InnovationDetailPage.tsx
+++ src/pages/innovations/InnovationDetailPage.tsx
@@ -11,13 +11,13 @@ const InnovationDetailPage: React.FC = () => {
       <Row className="w-100">
         <Col md={5} className="p-4 border-end ">
           <h3 className="card-title">Readme Viewer Section</h3>
-          <Container className="readmeViewer-content d-flex justify-content-center align-items-center min-vh-50 overflow-auto">
+          <Container className="readmeViewer-content border border-dark border-3   d-flex justify-content-center align-items-center min-vh-50 overflow-auto">
             <ReadmeViewer />
           </Container>   
         </Col>
         <Col md={7} className="p-4 ">
           <h3 className="card-title">Widget Viewer Section</h3>
-           <Container className="widgetViewer-content d-flex justify-content-center align-items-center min-vh-50 overflow-auto">
+           <Container className="widgetViewer-content d-flex border border-dark border-3 justify-content-center align-items-center min-vh-50 overflow-auto">
             <div>This is place holder for the widget interaction...</div>
           </Container> 
         </Col>
